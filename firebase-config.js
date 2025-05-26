a8643d5a420ac369c93cff3e2dc047feaacfc1cf
desbull <ts437556@gmail.com>
2025-05-27 00:21:19 +0530

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDxd9PwLLsxKMuA2ysJSoGUcwkzvBQ1xe0",
+  authDomain: "desibull-d6367.firebaseapp.com",
+  projectId: "desibull-d6367",
+  storageBucket: "desibull-d6367.firebasestorage.app",
+  messagingSenderId: "848276211708",
+  appId: "1:848276211708:web:2c286e13c60031b91ed479",
+  measurementId: "G-G62VNFB4R9"
+};
+
+export default firebaseConfig;
