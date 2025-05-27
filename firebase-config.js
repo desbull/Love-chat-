fd6fc1a7ae8e82d521d4dee89f4cc20a6628d32d
desbull <ts437556@gmail.com>
2025-05-27 09:57:46 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDxd9PwLLsxKMuA2ysJSoGUcwkzvBQ1xe0",
   authDomain: "desibull-d6367.firebaseapp.com",
   projectId: "desibull-d6367",
-  storageBucket: "desibull-d6367.firebasestorage.app",
+  storageBucket: "desibull-d6367.appspot.com",
   messagingSenderId: "848276211708",
   appId: "1:848276211708:web:2c286e13c60031b91ed479",
   measurementId: "G-G62VNFB4R9"
 };
 
-export default firebaseConfig;
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
