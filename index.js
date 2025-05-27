08fc89263df26925c8ef1bec6849e3dbe4074472
desbull <ts437556@gmail.com>
2025-05-27 16:22:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Placeholder content for index.js
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
