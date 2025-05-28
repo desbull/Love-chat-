0c07612928da9d399bac4889eb09ac0ad8866921
desbull <ts437556@gmail.com>
2025-05-28 10:39:02 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,24 +1,18 @@
-// Import Firebase functions
+// firebase-config.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getDatabase, ref, push, onValue } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-database.js";
 
-// (Optional) Import more services if needed:
-// import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
-// import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
-// import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-storage.js";
-
-// Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDxd9PwLLsxKMuA2ysJSoGUcwkzvBQ1xe0",
-  authDomain: "desibull-d6367.firebaseapp.com",
-  projectId: "desibull-d6367",
-  storageBucket: "desibull-d6367.appspot.com",
-  messagingSenderId: "848276211708",
-  appId: "1:848276211708:web:2c286e13c60031b91ed479",
-  measurementId: "G-G62VNFB4R9"
+  apiKey: "AIzaSyDxd9PwLLsxKMuA2ysJSoGUcwkzvBQ1xe0",
+  authDomain: "desibull-d6367.firebaseapp.com",
+  databaseURL: "https://desibull-d6367-default-rtdb.firebaseio.com",
+  projectId: "desibull-d6367",
+  storageBucket: "desibull-d6367.appspot.com",
+  messagingSenderId: "848276211708",
+  appId: "1:848276211708:web:2c286e13c60031b91ed479"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
 
-// Export app if needed elsewhere
-export default app;
+export { db, ref, push, onValue };
