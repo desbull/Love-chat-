af7786340d9e5f8362a7993c3df3bb14b3d8100a
desbull <ts437556@gmail.com>
2025-05-28 17:47:00 +0530

    Delete index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-// index.js
-import React from "https://esm.sh/react";
-import ReactDOM from "https://esm.sh/react-dom";
-import App from "./App.js";
-
-ReactDOM.render(<App />, document.getElementById("root"));
